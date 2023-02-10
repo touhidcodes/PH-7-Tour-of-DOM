cb1ba31b1bce2829b6c78bb4348bcb50c6ece570
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-10 11:18:03 +0600

    dom

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,17 @@
 console.log("hello js");
 console.log(document);
 console.log(document.body);
+
+const liCollection = document.getElementsByTagName("li");
+console.log(liCollection);
+
+for (const li of liCollection) {
+	console.log(li.innerText);
+}
+
+const heading = document.getElementsByTagName("h1");
+console.log(heading);
+
+for (const text of heading) {
+	console.log(text.innerText);
+}
