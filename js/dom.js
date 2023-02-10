c4788418e8d655537187478b664aed2bf6c6a3bb
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-10 12:34:08 +0600

    dom

--- js/dom.js
+++ js/dom.js
@@ -3,14 +3,14 @@ console.log(document);
 console.log(document.body);
 
 const liCollection = document.getElementsByTagName("li");
-console.log(liCollection);
+// console.log(liCollection);
 
 for (const li of liCollection) {
 	console.log(li.innerText);
 }
 
 const heading = document.getElementsByTagName("h1");
-console.log(heading);
+// console.log(heading);
 
 for (const text of heading) {
 	console.log(text.innerText);
@@ -18,8 +18,15 @@ for (const text of heading) {
 
 const fruits = document.getElementById("fruits-li");
 
-console.log(fruits.innerText);
+// console.log(fruits.innerText);
 
 for (const fruit of fruits) {
-	console.log(fruit);
+	console.log(fruit.innerText);
+}
+const visited = document.getElementsByClassName("visited");
+
+// console.log(visited.innerText);
+
+for (const visit of visited) {
+	console.log(visit.innerText);
 }
